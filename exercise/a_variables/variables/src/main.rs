b05d76519fd8a0eb8284373bf4e83ad0acfe825c
Forrest Walker <forrestpatwalker@hotmail.com>
2024-01-04 15:10:53 -0700

    solution for a_variables extra challenges

--- exercise/a_variables/variables/src/main.rs
+++ exercise/a_variables/variables/src/main.rs
@@ -2,12 +2,8 @@ const STARTING_MISSILES: i32 = 8;
 const READY_AMOUNT: i32 = 2;
 
 fn main() {
-    let mut missiles = STARTING_MISSILES;
-    let ready = READY_AMOUNT;
+    let (missiles, ready): (i32, i32) = (STARTING_MISSILES, READY_AMOUNT);
 
     println!("Firing {ready} of my {missiles} missiles....");
-
-    missiles = missiles - ready;
-
-    println!("{missiles} missiles left");
+    println!("{} missiles left", missiles - ready);
 }
